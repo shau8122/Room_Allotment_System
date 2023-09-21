098ee5abaf7ac38fed20df0b2dfb79131e54bf97
shau8122 <shau8122@kgpian.iitkgp.ac.in>
2023-09-21 21:12:13 +0530

    client url changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const app = express();
 
 
 connectToDatabase()
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
